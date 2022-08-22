175b80c81a6b68f5accb2b716d1255e6d4bd97d9
paigni <108606503+paigni@users.noreply.github.com>
2022-08-22 20:18:53 +0600

    Add files via upload

--- check.py
+++ check.py
@@ -1,4 +1,25 @@
 from urllib.parse import urlparse
-def is_valid(url):
+import urllib.request
+import urllib.error
+
+
+def check_is_up_url(url):
+    check_is_up = urllib.request.Request(url)
+    try:
+        urllib.request.urlopen(check_is_up)
+        return True
+    except urllib.error.URLError:
+        return False
+
+
+def is_valid_url(url):
     parsed = urlparse(url)
     return bool(parsed.netloc) and bool(parsed.scheme)
+
+
+def is_valid_arg(arg):
+    if 0 < arg < 999:
+        return True
+    return False
+
+# проверка на повтор
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-
+import sys
 from logic import get_urls
-from check import is_valid
+from check import is_valid_url,is_valid_arg,check_is_up_url
 print('Сборщик ссылок v1.0,перед запуском обратите внимание что вы должны ввести два аргумента'
       ' первый аргумент,ссылка на сам сайт по типу https://pythonworld.ru/ ,второй аргумент глубину погружения по ссылкам')
 
-base_url = input()
-pars_depth = input()
 
-def main(a,b):
-    if is_valid(b):
-        get_urls(b,a)
+def main():
+    base_url = sys.argv[1]
+    depth = int(sys.argv[2])
+    check_to_valid = is_valid_url(base_url)
+    check_is_up = check_is_up_url(base_url)
+    if check_to_valid and check_is_up:
+        if is_valid_arg(depth):
+            get_urls(base_url, depth)
     else:
         'Неверный ввод'
-main(base_url,pars_depth)
+
+
+if __name__ == '__main__':
+        main()
