ce1c0fb20dbc514ca7ecc144a1b4950bc6da5da6
paigni <108606503+paigni@users.noreply.github.com>
2022-08-23 20:38:59 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -12,18 +12,15 @@ def main():
     if len(sys.argv) == 3:
         base_url = sys.argv[1]
         depth = sys.argv[2]
-        if is_valid_url(base_url):
-            if is_valid_arg_2(depth):
-                if is_valid_args(base_url, int(depth)):
-                    get_urls(base_url, int(depth))
-            else:
-                sys.exit('Должно быть число,а не символ')
+        if is_valid_arg_2(depth):
+            if is_valid_args(base_url, int(depth)):
+                get_urls(base_url, int(depth))
         else:
-            sys.exit(f'Невалидная ссылка')
+            sys.exit('Должно быть число,а не символ')
     if len(sys.argv) == 2:
         base_url = sys.argv[1]
         depth = 3
-        if is_valid_args(base_url, depth):
+        if is_valid_url(base_url):
             get_urls(base_url, depth)
         else:
             sys.exit(f'Невалидная ссылка')
